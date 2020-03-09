2e5bf9cf9e58b1ef3c8d4e2cc49f483df470e057
AHFahad <53812182+AHFahad@users.noreply.github.com>
2020-03-09 13:46:22 +0600

    done

---
+++ db.php
@@ -0,0 +1,10 @@
+<?php
+
+ 
+$databaseHost = 'localhost';
+$databaseName = 'fahad';
+$databaseUsername = 'root';
+$databasePassword = '';
+ 
+$mysqli = mysqli_connect($databaseHost, $databaseUsername, $databasePassword, $databaseName); 
+?>
